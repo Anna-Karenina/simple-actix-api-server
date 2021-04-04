855d87276730f4053d271ac3e4eb28a2b87309cc
Анна Каренина <annakarenina@MBP-Anna.Dlink>
2021-04-04 15:27:40 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+mod config;
+mod db;
+mod handlers;
+mod models;
+
+use crate::handlers::{check_item, create_todo, get_items, get_todos, status};
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::io;
+use tokio_postgres::NoTls;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+  dotenv().ok();
+
+  let config = crate::config::Config::from_env().unwrap();
+  let pool = config.pg.create_pool(NoTls).unwrap();
+  println!(
+    "Starting server at http://{}:{}/",
+    config.server.host, config.server.port
+  );
+
+  HttpServer::new(move || {
+    App::new()
+      .data(pool.clone())
+      .route("/", web::get().to(status))
+      .route("/todos{_:/?}", web::get().to(get_todos))
+      .route("/todos{_:/?}", web::post().to(create_todo))
+      .route("/todos/{list_id}/items{_:/?}", web::get().to(get_items))
+      .route(
+        "/todos/{list_id}/items/{item_id}{_:/?}",
+        web::put().to(check_item),
+      )
+  })
+  .bind(format!("{}:{}", config.server.host, config.server.port))?
+  .run()
+  .await
+}
